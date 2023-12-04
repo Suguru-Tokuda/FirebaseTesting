993dd0bd029f108f9a76e9ea2467757de5558a60
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-12-04 08:42:25 -0500

    Initial Commit

---
+++ FirebaseTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTesting
+//
+//  Created by Suguru Tokuda on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
